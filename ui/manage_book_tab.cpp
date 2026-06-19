a1c6eee45a3fdda3a611e9fee3bfdce585f51b94
mynth05 <phamthithuy040506@gmail.com>
2026-06-19 15:31:48 +0700

    Update and rename manage_book_tab to manage_book_tab.cpp

---
+++ ui/manage_book_tab.cpp
@@ -0,0 +1,30 @@
+struct ManagaBookTab {
+    Table booktable;
+
+    Button addButton;
+    Button editButton;
+    Button deleteButton;
+    Button searchButton;
+
+    TextBox searchBox;
+};
+void build_manage_book_tab(App& app){
+
+    auto tab = app.createTab("Quan ly sach");
+
+    tab.addButton("Them sach");
+    tab.addButton("Cap nhat sach");
+    tab.addButton("Xoa sach");
+
+    tab.addTextBox("Tim kiem sach");
+
+    tab.addTable({
+        "Ma sach",
+        "Ten sach",
+        "Tac gia",
+        "The loai",
+        "So luong",
+        "Trang thai"
+        
+    });
+}
